386e1c5baea1a08533a0fca388652fea0fa36629
hotaosa <kfs.msl@gmail.com>
2023-01-03 18:10:57 +0900

    make CopyableText.tsx

---
+++ src/component/molecules/CopyableText.tsx
@@ -0,0 +1,21 @@
+import { Text, Tooltip, useClipboard } from '@chakra-ui/react';
+
+type Props = {
+  children: string;
+  prefix?: string;
+  fontSize?: string;
+  color?: string;
+};
+
+export const CopyableText = ((props: Props) => {
+  const { children, prefix = "", fontSize = "md", color = "gray.600"} = props;
+  const { onCopy, value, setValue } = useClipboard(children);
+
+  return (
+    <Tooltip label="Copy to clipboard">
+      <Text onClick={onCopy} fontSize={fontSize} color={color} _hover={{ cursor: "pointer", opacity: 0.8 }}>
+        {prefix}{children}
+      </Text>
+    </Tooltip>
+  );
+});
